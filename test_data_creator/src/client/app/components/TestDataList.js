54e6d9807feff433cbb30b17bb59c29a4754fd83
Rescian Rey <rescianrey@gmail.com>
2017-03-27 15:27:03 +0800

    So far so good

--- test_data_creator/src/client/app/components/TestDataList.js
+++ test_data_creator/src/client/app/components/TestDataList.js
@@ -16,23 +16,23 @@ export default class TestDataList extends React.Component {
     render() {
         var useTag = "<use xlink:href='" + ICON_CONTAINER + "custom-sprite/svg/symbols.svg#custom7')}'></use>";
         return (
-                <div>
-                    <div className="slds-page-header slds-m-bottom--small" role="banner">
-                        <div className="slds-media slds-media--center">
-                            <div className="slds-media__figure">
-                              <svg className="slds-icon slds-icon-standard-opportunity" aria-hidden="true" dangerouslySetInnerHTML={{__html: useTag }}>
-                              </svg>
-                            </div>
-                            <div className="slds-media__body">
-                                <p className="slds-page-header__title slds-truncate" title="Rohde Corp - 80,000 Widgets">All Test Data</p>
-                            </div>
-                            <div className="slds-media__figure slds-media__figure--reverse">
-                                <button className="slds-button slds-button--neutral" onClick={this.redirectToCreationPage}>New</button>
-                            </div>
+            <div>
+                <div className="slds-page-header slds-m-bottom--small" role="banner">
+                    <div className="slds-media slds-media--center">
+                        <div className="slds-media__figure">
+                          <svg className="slds-icon slds-icon-standard-opportunity" aria-hidden="true" dangerouslySetInnerHTML={{__html: useTag }}>
+                          </svg>
+                        </div>
+                        <div className="slds-media__body">
+                            <p className="slds-page-header__title slds-truncate" title="Rohde Corp - 80,000 Widgets">All Test Data</p>
+                        </div>
+                        <div className="slds-media__figure slds-media__figure--reverse">
+                            <button className="slds-button slds-button--neutral" onClick={this.redirectToCreationPage}>New</button>
                         </div>
                     </div>
-                    <TestDataTable />
                 </div>
+                <TestDataTable />
+            </div>
         );
     }
 }
